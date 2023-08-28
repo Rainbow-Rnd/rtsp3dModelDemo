e4632e78d6ef82dbfbadd3bead3cac4ced0cc273
Jamshid Khaytbaev <itsjk0307@gmail.com>
2023-08-28 13:59:07 +0900

    made link from env to show the video

--- src/VidoeLoader.jsx
+++ src/VidoeLoader.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import './'
 
 const VideoComponent = () => {
+  const videoUrl = process.env.REACT_APP_VIDEO_URL
   return (
     <div className="video-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <video controls>
-        <source src="/drone1.mp4" type="video/mp4" />
+        <source src={videoUrl} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
